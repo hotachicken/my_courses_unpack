21ceff34bf14c0ff4471c19a7eec2d7dc1f68d39
hotachicken <40753253+hotachicken@users.noreply.github.com>
2018-12-29 16:58:58 -0500

    Made funzip return a string, the new directory that was made, and made it possible to say exactly what new name to make the new file

--- unpack.py
+++ unpack.py
@@ -9,20 +9,26 @@ All extra files given are ignored
 import zipfile, sys, os
 CWD = os.getcwd()
 
-def funzip(file):
+def funzip(file, newname=None):
     """
     Unzip a file and put it into a folder of the same name
     :param file: str, file to be unzipped
-    :return: None
+    :return: str, the new directory
     """
     flen = len(file) #file length
     ploc = flen - 4 #location of period in '.zip'
 
-    os.mkdir(CWD + '/' + file[:ploc])
-    newdir = CWD + '/' + file[:ploc]
+    #we want to rename the file to something other than the folder name
+    if newname is not None:
+        os.mkdir(CWD + '/' + newname)
+        newdir = CWD + '/' + newname
+    else:
+        os.mkdir(CWD + '/' + file[:ploc])
+        newdir = CWD + '/' + file[:ploc]
     with zipfile.ZipFile(file, 'r') as zip:
         zip.printdir()
         zip.extractall(path=newdir)
+    return newdir
 
 def main():
     """
@@ -37,19 +43,32 @@ def main():
         print("usage: python %s file" % (sys.argv[0]), file=sys.stderr)
         exit(-1)
 
+    #mycourses file we are going to unzip
     filename = sys.argv[1]
+    print(filename)
     if len(filename) < 5:
         print("The length of the file was not long enough", file=sys.stderr)
+        print('In the case that the file has spaces in it,\
+         wrap the name with double quotes (") please', file=sys.stderr)
+        exit(-1)
 
     if filename[-4:] != ".zip": #ensure the file is a zip file
         print("file must be a zip file", file=sys.stderr)
         exit(-1)
 
+    #Unzip the first file
     try:
-        funzip(filename)
+        newdir = funzip(filename)
+
+        #unzip everything else
+        for file in newdir:
+            if file[-4:] == ".zip":
+                #TODO strip the numbers off the student's zip files
+                funzip(file)
     except:
         print("An error occured", file=sys.stderr)
         exit(-1)
+
 """
 Run the main
 """
