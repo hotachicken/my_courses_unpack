23801f5d9d8092f4186698a1403cc201a9bc7351
hotachicken <40753253+hotachicken@users.noreply.github.com>
2018-12-29 02:32:25 -0500

    Initial commit

---
+++ unpack.py
@@ -0,0 +1,17 @@
+"""
+Author: Matthew Rho
+file: unpack.py
+desc: A script to unpack student's files from mycourses
+"""
+
+"""
+Where all the good stuff happens
+"""
+def main():
+    pass
+
+"""
+Run the main
+"""
+if __name__ == "__main__":
+    main()
