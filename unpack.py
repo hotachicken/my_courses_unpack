96325f43695f34c252d92329cdbc223dd573a588
hotachicken <40753253+hotachicken@users.noreply.github.com>
2018-12-29 16:11:12 -0500

    Added file length to error checking, now creates a folder based on zip name, and unzips the said zip file

--- unpack.py
+++ unpack.py
@@ -7,13 +7,28 @@ All extra files given are ignored
 """
 
 import zipfile, sys, os
+CWD = os.getcwd()
 
-"""
-Where all the good stuff happens
-"""
+def funzip(file):
+    """
+    Unzip a file and put it into a folder of the same name
+    :param file: str, file to be unzipped
+    :return: None
+    """
+    flen = len(file) #file length
+    ploc = flen - 4 #location of period in '.zip'
+
+    os.mkdir(CWD + '/' + file[:ploc])
+    with zipfile.ZipFile(file, 'r') as zip:
+        zip.printdir()
+        zip.extractall()
+
 def main():
+    """
+    Where all the good stuff happens
+    :return: None
+    """
     argc = len(sys.argv)
-
     """
     Error checking, for you maniacs out there
     """
@@ -21,10 +36,19 @@ def main():
         print("usage: python %s file" % (sys.argv[0]), file=sys.stderr)
         exit(-1)
 
-    if sys.argv[1][-4:] != ".zip": #ensure the file is a zip file
+    filename = sys.argv[1]
+    if len(filename) < 5:
+        print("The length of the file was not long enough", file=sys.stderr)
+
+    if filename[-4:] != ".zip": #ensure the file is a zip file
         print("file must be a zip file", file=sys.stderr)
         exit(-1)
 
+    try:
+        funzip(filename)
+    except:
+        print("An error occured", file=sys.stderr)
+        exit(-1)
 """
 Run the main
 """
